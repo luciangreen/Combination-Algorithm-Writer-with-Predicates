bc40b80e7a64a04a98238ede469b2175aeec3939
Lucian Green <luciangreen@lucianacademy.com>
2019-07-20 21:48:39 +1000

    rcawp randomly writes algorithms with predicates

--- listprolog.pl
+++ listprolog.pl
@@ -4,5 +4,6 @@
 :- include('lpiverify4.pl').
 :- include('caw5 copy 12.pl').
 :- include('cawpverify.pl').
+:- include('rcawp.pl').
 %%:- include('rcaw.pl').
-%%:- include('texttobr2.pl').
+:- include('texttobr2.pl').
