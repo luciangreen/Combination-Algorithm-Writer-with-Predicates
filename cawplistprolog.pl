edfb1c0732a4247401c055fb86ba6b1e63b2c444
Lucian Green <luciangreen@lucianacademy.com>
2020-03-22 20:26:47 +1100

    Update for LPPM

--- cawplistprolog.pl
+++ cawplistprolog.pl
@@ -1,10 +1,11 @@
-:- include('grammar.pl').
+:- include('../listprologinterpreter/grammar.pl').
 %%:- include('lpi_caw_commands.pl').
-:- include('listprologinterpreter1listrecursion4.pl').
-:- include('listprologinterpreter3preds5.pl').
-:- include('lpiverify4.pl').
+:- include('../listprologinterpreter/listprologinterpreter1listrecursion4.pl').
+:- include('../listprologinterpreter/listprologinterpreter3preds5.pl').
+:- include('../listprologinterpreter/lpiverify4.pl').
 :- include('caw5 copy 12.pl').
 :- include('cawpverify.pl').
 :- include('rcawp.pl').
 %%:- include('rcaw.pl').
-:- include('texttobr2.pl').
+:- include('la_strings.pl').
+%%:- include('texttobr2.pl').
